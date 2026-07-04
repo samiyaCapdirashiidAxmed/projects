ea848221ffecdc7c8c5d5f84e95913610c3b3d07
samiyaCapdirashiidAxmed <samiyacapdirashiid@gmail.com>
2026-07-04 08:49:56 +0300

    first commit

---
+++ HOME.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <center>
+        <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coffee websit </title>
+    <link rel="stylesheet" href="style.css" >
+    </head>
+   
+    <body >
+         
+        <!--header/navbar-->
+        <header>
+            <nav class="navbar section-contnant">
+                <a href="#" class="nav-logo">
+                    <h2 class="logo-text">hospital  </h2>
+                </a>
+                <ul class="nav-menu">
+                    <li class="nav-item">
+                        <a href="registration.php" class="nav-link">registration</a>
+                    </li>
+                </ul>
+                
+                <ul class="nav-menu">
+                    <li class="nav-item">
+                        <a href="logo.php" class="nav-link">logING </a>
+                    </li>
+                </ul>
+                <ul class="nav-menu">
+                    <li class="nav-item">
+                        <a href="Edit.php" class="nav-link">EDINT</a>
+                    </li>
+                </ul>
+                <ul class="nav-menu">
+                    <li class="nav-item">
+                        <a href="DELET.php" class="nav-link">APPOMENT</a>
+                    </li>
+                </ul>
+                <ul class="nav-menu">
+                    <li class="nav-item">
+                        <a href="gallery.html" class="nav-link">DECTORE </a>
+                    </li>
+                </ul>
+                <ul class="nav-menu">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">bBOOKING</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        
+        <main>
+            <!--hero section-->
+            <sectioc class="hero-section">
+                <div class="section-contenat">
+                    <div class="hero-details">
+                       <p> <h2 class="titel">BEST HOSPITAL</h2>
+                        <h3 class="description">Make your life better with our special healthcare.</h3>
+                        <p class="description">Welcome to our hospital, where every patient matters and every treatment brings hope and healing.</p>
+      </p>
+                        <div class="buttons">
+            <a href="logo.php" class="buttons order-now">loging</a><br>
+          
+        </div>      
+        
+                    </div>
+                    <div class="hero-image-wrapper">
+                <!--<img src="image.png" alt="hero" class="hero-image"> -->
+                    </div>
+                </div>
+               <!--<img src="ww.gbj.jpg" alt="hero" class="hero-image"> -->
+            </sectioc>
+        </main>
+    </body></center>
+
+    <footer>
+                                            All Copy Right is ISKAASHI GROUP
+                                                                                                                                                                                                                                                                                      
+
+    </footer>
+</html>
