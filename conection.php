1b481bb1e93c1cacfe0127ce14e8aa4c930ee0bb
samiyaCapdirashiidAxmed <samiyacapdirashiid@gmail.com>
2026-07-05 10:11:02 +0300

    first commit

---
+++ conection.php
@@ -0,0 +1,15 @@
+<?php 
+
+$SERVER="localhost";
+$user="root";
+$password="";
+$database="software_project_management1";//dataBASE NAME 
+$con=mysqli_connect($SERVER,$user,$password,$database);// 
+if($con){
+echo"";
+
+}
+else{
+    echo"";
+}
+?>
